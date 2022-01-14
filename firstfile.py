f51c114a1231aa1d586e6e6974db6a743668a1f6
sepehrcyrusian <70033987+sepehrcyrusian@users.noreply.github.com>
2022-01-14 21:51:16 +0100

    Update firstfile.py

--- firstfile.py
+++ firstfile.py
@@ -2,4 +2,5 @@ from os import write
 from typing import Sequence
 import streamlit as st
 
-st.write("My cool secrets:", st.secrets["things_i_like"]) 
+for i in range(len(st.secrets["things_i_like"])):
+  print(i)
