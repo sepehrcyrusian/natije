bb37f886cd341a25b7737e6bd79be56158a77cbf
sepehrcyrusian <70033987+sepehrcyrusian@users.noreply.github.com>
2022-01-14 21:48:06 +0100

    Update firstfile.py

--- firstfile.py
+++ firstfile.py
@@ -1,6 +1,5 @@
 from os import write
 from typing import Sequence
 import streamlit as st
-from sentimentorr import sentimentor
-from demojz import demojj
-from annotator import ignitor_load,appendor,allll
+
+print(things_i_like) 
